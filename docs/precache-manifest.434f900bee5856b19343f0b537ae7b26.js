7cb5ffd0f166bd61324878288201759da26b78bb
Gabriel Rodriguez <isclgrl@gmal.com>
2020-10-27 14:39:36 -0600

    docs agregado

---
+++ docs/precache-manifest.434f900bee5856b19343f0b537ae7b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9862ad5c9995af7c490ac9542f55a18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6de8168410ad9a742033",
+    "url": "/static/css/main.dd618ffd.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de8168410ad9a742033",
+    "url": "/static/js/main.1a3d293a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
